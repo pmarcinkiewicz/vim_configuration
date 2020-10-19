f3cea81a07eaed49e603be8f7bd33626b197eb9d
Piotr Marcinkiewicz <pmarcinkiewicz@darkspacelab.com>
2020-10-19 18:30:04 +0200

    JEDI completion added

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-apt-get update && apt-get install -y vim cscope ctags myspell-en-gb myspell-pl
+apt-get update && apt-get install -y vim cscope ctags hunspell-en-gb hunspell-pl git
+pip3 install jedi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
 cp -r .vim ~/
 cp -r .vimrc ~/
